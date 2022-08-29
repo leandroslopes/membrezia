7c69cd861a1774683641f02f479016d8287d0035
Leandro S Lopes <programador.leandrolopes@gmail.com>
2022-08-29 11:28:55 +0000

    Changed URL base

--- source/util/SystemUtil.php
+++ source/util/SystemUtil.php
@@ -4,5 +4,6 @@ class SystemUtil
 {
     const TITLE_SYSTEM = "MEMBREZIA";
     const SUBTITLE_SYSTEM = "Sistema de Gerenciamento de Ministério v.02";
-    const URL_SYSTEM = "http://localhost/membrezia/";
+    //const URL_SYSTEM = "http://localhost/membrezia/";
+    const URL_SYSTEM = "https://membrezia.herokuapp.com/";
 }
